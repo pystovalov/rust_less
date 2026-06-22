2412ab9142d919b42084d3c381addd4cdca722cd
Alexander <a.a.pystovalov@yandex.tu>
2026-06-22 13:20:48 +0300

    001 cargo new

---
+++ p001_cargo_new/target/debug/deps/p001_cargo_new-e7a06ec79aaf855c.d
@@ -0,0 +1,5 @@
+/home/alexander/code/rust/less/oreilly/p001_cargo_new/target/debug/deps/p001_cargo_new-e7a06ec79aaf855c.d: src/main.rs
+
+/home/alexander/code/rust/less/oreilly/p001_cargo_new/target/debug/deps/p001_cargo_new-e7a06ec79aaf855c: src/main.rs
+
+src/main.rs:
---
+++ p001_cargo_new/target/debug/p001_cargo_new.d
@@ -0,0 +1,1 @@
+/home/alexander/code/rust/less/oreilly/p001_cargo_new/target/debug/p001_cargo_new: /home/alexander/code/rust/less/oreilly/p001_cargo_new/src/main.rs
