9a69746cb4e1af6128c437dc4c12aa9cd0001957
Alexander <a.a.pystovalov@yandex.tu>
2026-06-22 15:31:17 +0300

    006 loop

--- p006_control_flow/src/main.rs
+++ p006_control_flow/src/main.rs
@@ -1,4 +1,9 @@
 use std::io;
+macro_rules! line {
+    () => {
+        println!("=============");
+    };
+}
 macro_rules! control_flow {
     ($number:expr) => {
         if $number < 0 {
@@ -11,13 +16,22 @@ macro_rules! control_flow {
     };
 }
 fn main() {
-    println!("Please number positive, negative or zero: ");
-    let mut number = String::new();
-    io::stdin().read_line(&mut number).expect("Failed");
-    let number: i32 = number.trim().parse().expect("Please enter a number");
-    control_flow!(number);
-
-    if number % 2 == 0 && number > 0 {
-        println!("Number devided to 2");
+    let mut count = 0;
+    loop {
+        count += 1;
+        println!("Please number positive, negative or zero: ");
+        let mut number = String::new();
+        io::stdin().read_line(&mut number).expect("Failed");
+        let number: i32 = number.trim().parse().expect("Please enter a number");
+        control_flow!(number);
+        if number % 2 == 0 && number > 0 {
+            println!("Number devided to 2");
+        }
+        if count == 5 {
+            break;
+        }
     }
+    line!();
+    println!("EXIT");
+    line!();
 }
