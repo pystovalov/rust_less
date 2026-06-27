ef7501e31d4fd22b75795c0f10b6df7562d0121e
Alexander <a.a.pystovalov@yandex.tu>
2026-06-27 23:28:14 +0300

    hashmap from iterator

--- p017_HashMap/src/main.rs
+++ p017_HashMap/src/main.rs
@@ -4,11 +4,22 @@ macro_rules! line {
         println!("----------------");
     };
 }
+macro_rules! print_var {
+    ($ver:expr) => {
+        println!("{:?}", $ver);
+    };
+}
 
 fn main() {
     let mut student_grades = HashMap::new();
     student_grades.insert("Alice", 84);
     student_grades.insert("Bob", 34);
-    println!("{:?}", student_grades);
+    print_var!(student_grades);
+    line!();
+
+    let names = vec!["Alice", "Bob", "Charlie"];
+    let grades = vec![23, 58, 20];
+    let students_grades: HashMap<_, _> = names.into_iter().zip(grades.into_iter()).collect();
+    print_var!(students_grades);
     line!();
 }
