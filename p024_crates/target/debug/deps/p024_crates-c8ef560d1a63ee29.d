cf6db54e260adfd04496e3c297fadcf144493dfc
Alexander <a.a.pystovalov@yandex.tu>
2026-07-14 11:39:48 +0300

    start crates

---
+++ p024_crates/target/debug/deps/p024_crates-c8ef560d1a63ee29.d
@@ -0,0 +1,5 @@
+/home/alexander/code/rust/less/oreilly/p024_crates/target/debug/deps/p024_crates-c8ef560d1a63ee29.d: src/main.rs
+
+/home/alexander/code/rust/less/oreilly/p024_crates/target/debug/deps/libp024_crates-c8ef560d1a63ee29.rmeta: src/main.rs
+
+src/main.rs:
---
+++ p024_crates/target/debug/deps/p024_crates-ef866863ae656644.d
@@ -0,0 +1,5 @@
+/home/alexander/code/rust/less/oreilly/p024_crates/target/debug/deps/p024_crates-ef866863ae656644.d: src/main.rs
+
+/home/alexander/code/rust/less/oreilly/p024_crates/target/debug/deps/libp024_crates-ef866863ae656644.rmeta: src/main.rs
+
+src/main.rs:
